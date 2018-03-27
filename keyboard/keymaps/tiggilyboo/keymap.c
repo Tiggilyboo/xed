d275b57b3f018a4e1355aae40ecc0bea52ab6157
Simon Willshire <me@simonwillshire.com>
2018-03-28 11:44:17 +1300

    Updated keymap

--- keyboard/keymaps/tiggilyboo/keymap.c
+++ keyboard/keymaps/tiggilyboo/keymap.c
@@ -26,7 +26,9 @@ enum planck_keycodes {
   SLEEP,
   WRKSPC,
   XEDDEV,
-  CHROME
+  XAHF,
+  XASP,
+  CHROME,
 };
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
@@ -45,28 +47,28 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
    * | Shift|   Z  |   X  |   C  |   V  |   B  |   K  |   M  |   ,  |   .  |   /  |Enter |
    * |      |  F7  |  F8  |  F9  |  F10 |  F11 |  F12 |ISO ~ |ISO | | Vol- | Vol+ |      |
    * |------+------+------+------+------+------+------+------+------+------+------+------|
-   * | Scr- | Scr+ |      |      |      | Vol- | Vol+ |      | Home |Pg Up |Pg Dn | End  |
+   * |      |      |      |      |      | Scr- | Scr+ |      | Home |Pg Up |Pg Dn | End  |
    * | Ctrl | GUI  | Alt  |WrkSp |Lower |Space | Bksp |Raise | Left | Down |  Up  |Right |
-   * | Scr- | Scr+ |      |      |      | Vol- | Vol+ |      | Home |Pg Up |Pg Dn | End  |
+   * |      |      |      |      |      | Vol- | Vol+ |      | Home |Pg Up |Pg Dn | End  |
    * `-----------------------------------------------------------------------------------'
    */
   [_BASE] = {
-    {KC_TAB,  KC_Q,    KC_W,    KC_F,    KC_P,    KC_G,    KC_J,    KC_L,    KC_U,    KC_Y,    KC_SCLN, KC_BSPC},
-    {KC_ESC,  KC_A,    KC_R,    KC_S,    KC_T,    KC_D,    KC_H,    KC_N,    KC_E,    KC_I,    KC_O,    KC_QUOT},
-    {KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_K,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_ENT },
-    {KC_LCTL, KC_LGUI, KC_LALT, WRKSPC,  LOWER,   KC_SPC,  KC_BSPC, RAISE,   KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT}
+    {KC_TAB,  KC_Q,    KC_W,    KC_F,    KC_P,    KC_G,    KC_J,    KC_L,       KC_U,       KC_Y,    KC_SCLN, KC_BSPC},
+    {KC_ESC,  KC_A,    KC_R,    KC_S,    KC_T,    KC_D,    KC_H,    KC_N,       KC_E,       KC_I,    KC_O,    KC_QUOT},
+    {KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_K,    KC_M,       KC_COMM,    KC_DOT,  KC_SLSH, KC_ENT },
+    {KC_LCTL, KC_LGUI, KC_LALT, WRKSPC,  LOWER,   KC_SPC,  KC_BSPC, RAISE,      KC_LEFT,    KC_DOWN, KC_UP,   KC_RGHT}
   },
   [_LOWER] = {
     {KC_TILD, KC_EXLM, KC_AT,   KC_HASH, KC_DLR,  KC_PERC, KC_CIRC, KC_AMPR,    KC_ASTR,    KC_LPRN, KC_RPRN, KC_BSPC},
     {KC_DEL,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_UNDS,    KC_PLUS,    KC_LCBR, KC_RCBR, KC_PIPE},
-    {_______, KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  S(KC_NUHS), S(KC_NUBS), _______, _______, KC_F8},
-    {KC_F6,   KC_F7,   _______, _______, _______, KC_F9,   KC_F10,  _______,    KC_HOME,    KC_PGDN, KC_PGUP, KC_END }
+    {KC_LSFT, KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  S(KC_NUHS), S(KC_NUBS), _______, _______, KC_ENT },
+    {KC_LCTL, _______, _______, _______, _______, KC_F9,   KC_F10,  _______,    KC_HOME,    KC_PGDN, KC_PGUP, KC_END }
   },
   [_RAISE] = {
-    {KC_GRV,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_BSPC},
-    {KC_DEL,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_MINS, KC_EQL,  KC_LBRC, KC_RBRC, KC_BSLS},
-    {_______, KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  KC_NUHS, KC_NUBS, _______, _______, KC_F8},
-    {KC_F6,   KC_F7,   _______, _______, _______, KC_F9,   KC_F10,  _______,    KC_HOME,    KC_PGDN, KC_PGUP, KC_END }
+    {KC_GRV,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,       KC_8,       KC_9,    KC_0,    KC_BSPC},
+    {KC_DEL,  _______, _______, _______, _______, _______, _______, KC_MINS,    KC_EQL,     KC_LBRC, KC_RBRC, KC_BSLS},
+    {KC_LSFT, _______, _______, _______, _______, _______, _______, KC_NUHS,    KC_NUBS,    _______, _______, KC_ENT },
+    {KC_LCTL, _______, _______, _______, _______, KC_F6,   KC_F7,   _______,    KC_HOME,    KC_PGDN, KC_PGUP, KC_END }
   },
 
   /* Adjust (Lower + Raise)
@@ -77,16 +79,27 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
    * |------+------+------+------+------+------|------+------+------+------+------+------|
    * | BL-  |Voice-|Voice+|Mus on|Musoff|MIDIon|MIDIof|      |      |      |      | Dev  |
    * |------+------+------+------+------+------+------+------+------+------+------+------|
-   * | BL   |      |      |      |      |             |      |      |      |      |      |
+   * | BL   |      |      |      |      |             |      |      |      |      |EditMe|
    * `-----------------------------------------------------------------------------------'
    */
   [_ADJUST] = {
-    {SLEEP,  RESET,   DEBUG,   RGB_TOG, RGB_MOD, RGB_HUI, RGB_HUD, RGB_SAI, RGB_SAD, RGB_VAI, RGB_VAD, KC_F11 },
+    {SLEEP,  RESET,   DEBUG,   RGB_TOG, RGB_MOD, RGB_HUI, RGB_HUD, RGB_SAI, RGB_SAD, RGB_VAI, RGB_VAD,  KC_SYSTEM_POWER },
     {BLINC,  _______, MU_MOD,  AU_ON,   AU_OFF,  AG_NORM, AG_SWAP, _______, _______, _______, _______,  CHROME},
     {BLDEC,  MUV_DE,  MUV_IN,  MU_ON,   MU_OFF,  MI_ON,   MI_OFF,  TERM_ON, TERM_OFF, _______, _______, XEDDEV },
-    {BLTGL,  _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______}
+    {BLTGL,  _______, _______, _______, _______, XASP,    XAHF,    _______, _______, _______, _______, _______}
   }
 };
+
+void process_altdcmd(char* command){
+  register_code(KC_LALT);
+  register_code(KC_D);
+  unregister_code(KC_D);
+  unregister_code(KC_LALT);
+  wait_ms(100);
+  send_string(command);
+  register_code(KC_ENT);
+  unregister_code(KC_ENT);
+}
 
 bool process_record_user(uint16_t keycode, keyrecord_t *record) {
   switch (keycode) {
@@ -148,35 +161,37 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
       break;
     case CHROME:
       if(record->event.pressed){
-        register_code(KC_LALT);
-        register_code(KC_D);
-        unregister_code(KC_D);
-        unregister_code(KC_LALT);
-        wait_ms(100);
-        SEND_STRING("google-chrome");
-        register_code(KC_ENT);
-        unregister_code(KC_ENT);
+        process_altdcmd("google-chrome");
       }
+      break;
+    case XASP:
+      if (record->event.pressed) {
+        process_altdcmd("xasp");
+      }
+      return false;
+      break;
+    case XAHF:
+      if (record->event.pressed) {
+        process_altdcmd("xahf");
+      }
+      return false;
       break;
   #ifdef BACKLIGHT_ENABLE
     case BLTGL:
       if (record->event.pressed) {
-        backlight_toggle();
-        breathing_disable();
+        breathing_toggle();
       }
       return false;
       break;
     case BLINC:
       if (record->event.pressed) {
         backlight_increase();
-        breathing_disable();
       }
       return false;
       break;
     case BLDEC:
       if (record->event.pressed) {
         backlight_decrease();
-        breathing_disable();
       }
       return false;
       break;
